94f3bcf996cbf6ddfe13f778d0e6534109570265
Jannis R <mail@jannisr.de>
2016-04-12 00:40:53 +0200

    implementation :clap:

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const path     = require('path')
+const tokenize = require('vbb-tokenize-station')
+const fs       = require('fs')
+const ndjson   = require('ndjson')
+const filter   = require('stream-filter')
+
+
+
+const file = path.join(__dirname, 'stations.ndjson')
+
+// naming:
+// - a fragment is a part of a search query
+// - a token is a part of a stations' name
+// - a result is a triple of station, relevance & weight
+
+
+
+const autocomplete = function (query) {
+	const results = []
+	const fragments = tokenize(query)
+	if (fragments.length === 0) return []
+
+	return fs.createReadStream(file)
+	.pipe(ndjson.parse())
+	.pipe(filter((station) => {
+		for (let fragment of fragments) {
+			if (station.tokens.indexOf(fragment) < 0)
+				return false
+		}
+		return true
+	}))
+}
+
+module.exports = autocomplete
