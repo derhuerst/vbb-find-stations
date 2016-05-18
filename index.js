f09aeeb59677a1e253c60fe86b212fb7494ada72
Jannis R <mail@jannisr.de>
2016-05-19 01:07:25 +0200

    clean slate

--- index.js
+++ index.js
@@ -1,47 +1,9 @@
 'use strict'
 
+const fs       = require('fs')
 const path     = require('path')
+const ndjson   = require('ndjson')
+const stream   = require('stream')
 const tokenize = require('vbb-tokenize-station')
 const leven    = require('leven')
-const fs       = require('fs')
-const ndjson   = require('ndjson')
 const filter   = require('stream-filter')
-
-
-
-const file = path.join(__dirname, 'stations.ndjson')
-
-// naming:
-// - a fragment is a part of a search query
-// - a token is a part of a stations' name
-// - a result is a triple of station, relevance & weight
-
-
-
-const match = (fragments) => {
-	if (fragments.length === 0) return () => false
-	const joined = fragments.join(' ')
-	return (station) => {
-		let result = true
-		for (let fragment of fragments) {
-			if (station.tokens.indexOf(fragment) < 0) {
-				result = false
-				break
-			}
-		}
-		if (leven(joined, station.tokens.join(' ')) < 3) result = true
-		return result
-	}
-}
-
-const find = (query) => {
-	const results = []
-	const fragments = tokenize(query)
-	if (fragments.length === 0) return []
-
-	return fs.createReadStream(file)
-	.pipe(ndjson.parse())
-	.pipe(filter(match(fragments)))
-}
-
-module.exports = Object.assign(find, {match})
--- test.js
+++ test.js
@@ -1,28 +1,4 @@
 'use strict'
 
-const a              = require('assert')
-const find          = require('./index.js')
-
-let test1 = 0
-find('U Berliner Str. (Berlin)')
-.on('data', (station) => {
-	a.strictEqual(typeof station,    'object')
-	a.strictEqual(typeof station.id, 'number')
-	a.strictEqual(station.name, 'U Berliner Str. (Berlin)')
-	test1++
-})
-.on('end', () => {
-	a.ok(test1 > 0, 'No results.')
-})
-
-let test2 = 0
-find('U Alt-Mareindorf (Berlin)') // typo
-.on('data', (station) => {
-	a.strictEqual(typeof station,    'object')
-	a.strictEqual(typeof station.id, 'number')
-	a.strictEqual(station.name, 'U Alt-Mariendorf (Berlin)')
-	test2++
-})
-.on('end', () => {
-	a.ok(test2 > 0, 'No results.')
-})
+const test = require('tape')
+const find = require('./index')
