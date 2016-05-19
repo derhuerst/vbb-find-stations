532cde738f0063f051bfc41388b1993ac7bdfa02
Jannis R <mail@jannisr.de>
2016-05-19 02:09:54 +0200

    implementation :clap:

--- index.js
+++ index.js
@@ -4,6 +4,63 @@ const fs       = require('fs')
 const path     = require('path')
 const ndjson   = require('ndjson')
 const stream   = require('stream')
-const tokenize = require('vbb-tokenize-station')
-const leven    = require('leven')
 const filter   = require('stream-filter')
+const through  = require('through2')
+const leven    = require('leven')
+const tokenize = require('vbb-tokenize-station')
+
+
+
+const exact = (query) => filter((station) => {
+	const tokens = Array.from(station.tokens)
+	for (let fragment of query) {
+		const i = tokens.indexOf(fragment)
+		if (i < 0) {tokens.splice(i, 1); return false}
+	}
+	return true
+})
+
+
+
+const fuzzy = (query) => through.obj(function (station, _, cb) {
+	const tokens = Array.from(station.tokens)
+	let relevance = 0
+	for (let fragment of query) {
+
+		let i = tokens.indexOf(fragment) // try exact matching
+		if (i < 0) { // try fuzzy matching
+			let distance = 3
+			for (let j = 0; j < tokens.length; j++) {
+				const d = leven(tokens[j], fragment)
+				if (d < distance) {distance = d; i = j}
+			}
+		}
+
+		if (i >= 0) {relevance += 1; tokens.splice(i, 1)}
+		else return cb() // fragment not found in tokens
+	}
+	station = Object.create(station)
+	station.relevance = relevance
+	this.push(station)
+	cb()
+})
+
+
+
+const find = (query, filter) => {
+	query = tokenize(query)
+	if (query.length === 0) return empty()
+	return fs.createReadStream(path.join(__dirname, 'stations.ndjson'))
+	.pipe(ndjson.parse())
+	.pipe(filter(query))
+}
+
+const empty = () => {
+	const s = new stream.PassThrough({objectMode: true})
+	s.end()
+	return s
+}
+
+
+
+module.exports = Object.assign(find, {exact, fuzzy})
