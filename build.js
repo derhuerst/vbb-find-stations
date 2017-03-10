7dd7624236c549a5c0b6e4424bd498d86272a301
Jannis R <mail@jannisr.de>
2017-03-10 22:06:19 +0100

    code style :shirt:

--- build.js
+++ build.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const ndjson   = require('ndjson')
-const fs       = require('fs')
-const path     = require('path')
-const common   = require('vbb-common-places').stations
+const ndjson = require('ndjson')
+const fs = require('fs')
+const path = require('path')
+const common = require('vbb-common-places').stations
 const stations = require('vbb-stations/full.json')
 const tokenize = require('vbb-tokenize-station')
 
@@ -13,11 +13,14 @@ const showError = (err) => {
 	process.exit(1)
 }
 
-
+const dest = path.join(__dirname, 'stations.ndjson')
 
 const out = ndjson.stringify()
-out.pipe(fs.createWriteStream(path.join(__dirname, 'stations.ndjson')))
-.on('error', showError).on('finish', () => console.log('done'))
+out.pipe(fs.createWriteStream(dest))
+.on('error', showError)
+.on('finish', () => console.log('done'))
+
+
 
 for (let alias in common) {
 	const station = stations[common[alias]]
@@ -26,9 +29,9 @@ for (let alias in common) {
 		continue
 	}
 	out.write({
-		  id:     station.id
-		, name:   station.name
-		, tokens: tokenize(alias)
+		id: station.id,
+		name: station.name,
+		tokens: tokenize(alias)
 	})
 }
 
@@ -39,9 +42,9 @@ for (let id in stations) {
 		continue
 	}
 	out.write({
-		  id:     station.id
-		, name:   station.name
-		, tokens: tokenize(station.name)
+		id: station.id,
+		name: station.name,
+		tokens: tokenize(station.name)
 	})
 }
 
