3e13117249a70fc7c48ce9aa1dc76e922a0cf7f8
Jannis R <mail@jannisr.de>
2017-03-31 12:54:29 +0200

    use vbb-stations-autocomplete, adapt tests :white_check_mark:

--- index.js
+++ index.js
@@ -1,75 +1,21 @@
 'use strict'
 
-const fs       = require('fs')
-const path     = require('path')
-const filter   = require('stream-filter')
-const through  = require('through2')
-const leven    = require('leven')
-const tokenize = require('vbb-tokenize-station')
-const ndjson   = require('ndjson')
-const stream   = require('stream')
+const stream = require('stream')
+const autocomplete = require('vbb-stations-autocomplete')
 
 
 
-const exact = (query) => through.obj(function (station, _, cb) {
-	const tokens = Array.from(station.tokens)
-	for (let fragment of query) {
-		const i = tokens.indexOf(fragment)
-		if (i < 0) {
-			tokens.splice(i, 1)
-			return cb()
-		}
-	}
-
-	station = Object.assign({type: 'station'}, station)
-	this.push(station)
-	cb()
-})
-
-
-
-const fuzzy = (query) => through.obj(function (station, _, cb) {
-	const tokens = Array.from(station.tokens)
-	let relevance = .5 / station.name.length
-	for (let fragment of query) {
-
-		let i = tokens.indexOf(fragment) // try exact matching
-		if (i >= 0) {relevance += 1; tokens.splice(i, 1); continue}
-
-		let distance = 3
-		for (let j = 0; j < tokens.length; j++) {
-			const d = leven(tokens[j], fragment)
-			if (d < distance) {distance = d; i = j}
-		}
-		if (i >= 0) {
-			relevance += 1 / (distance + 1)
-			tokens.splice(i, 1)
-			continue
-		}
-
-		return cb() // fragment not found in tokens
-	}
-	station = Object.assign({type: 'station', relevance}, station)
-	this.push(station)
-	cb()
-})
-
-
+const exact = false
+const fuzzy = true
 
 const find = (query, filter = exact) => {
-	query = tokenize(query)
-	if (query.length === 0) return empty()
-	return fs.createReadStream(path.join(__dirname, 'stations.ndjson'))
-	.pipe(ndjson.parse())
-	.pipe(filter(query))
+	const out = new stream.PassThrough({objectMode: true})
+
+	const stations = autocomplete(query, 100, !!filter, false)
+	for (let station of stations) out.write(station)
+	out.end()
+
+	return out
 }
-
-const empty = () => {
-	const s = new stream.PassThrough({objectMode: true})
-	s.end()
-	return s
-}
-
-
 
 module.exports = Object.assign(find, {exact, fuzzy})
--- test.js
+++ test.js
@@ -1,93 +1,26 @@
 'use strict'
 
-const test = require('blue-tape')
-const so   = require('so')
+const test = require('tape')
 const find = require('.')
 
-const sink = (stream, input) => new Promise((yay, nay) => {
-	const data = []
-	stream
-	.on('data', (d) => data.push(d))
-	.on('error', (err) => nay(err))
-	.on('end', () => yay(data))
-	stream.write(input)
-	stream.end()
+test('find – U Alt-Mareindorf', (t) => {
+	find('U Alt-Mareindorf', find.fuzzy) // note the typo
+	.on('data', (s) => {
+		t.equal(s.type, 'station')
+		if (s.id === '900000070301') {
+			t.pass('match')
+			t.end()
+		}
+	})
 })
 
-
-
-test('exact: more fragments than tokens', so(function* (t) {
-	const s = find.exact(['foo', 'bar'])
-	const i = {type: 'station', name: 'Foo', tokens: ['foo']}
-	t.deepEqual(yield sink(s, i), [])
-}))
-
-test('exact: typo', so(function* (t) {
-	const s = find.exact(['foo', 'bar'])
-	const i = {type: 'station', name: 'Foo (Baz)', tokens: ['foo', 'baz']}
-	t.deepEqual(yield sink(s, i), [])
-}))
-
-test('exact: less fragments than tokens', so(function* (t) {
-	const s = find.exact(['foo'])
-	const i = {type: 'station', name: 'Foo (Bar)', tokens: ['foo', 'bar']}
-	t.deepEqual(yield sink(s, i), [i])
-}))
-
-test('exact: as many fragments as tokens', so(function* (t) {
-	const s = find.exact(['foo', 'bar'])
-	const i = {type: 'station', name: 'Foo (Bar)', tokens: ['foo', 'bar']}
-	t.deepEqual(yield sink(s, i), [i])
-}))
-
-
-
-test('fuzzy: 1 fragment, 1 token, distance of 3', so(function* (t) {
-	const s = find.fuzzy(['bar'])
-	const i = {type: 'station', name: 'Foo', tokens: ['foo']}
-	t.deepEqual(yield sink(s, i), [])
-}))
-
-test('fuzzy: 1 fragment, 1 token, distance of 2', so(function* (t) {
-	const s = find.fuzzy(['bar'])
-	const i = {type: 'station', name: 'Buz', tokens: ['buz']}
-	const r = yield sink(s, i)
-	t.equal(r.length, 1)
-	t.equal(r[0].name, i.name)
-	t.equal(r[0].tokens, i.tokens)
-	t.equal(r[0].relevance, 1/3 + .5/'Buz'.length)
-}))
-
-test('fuzzy: 1 fragment, 1 token, distance of 1', so(function* (t) {
-	const s = find.fuzzy(['bar'])
-	const i = {type: 'station', name: 'Baz', tokens: ['baz']}
-	const r = yield sink(s, i)
-	t.equal(r.length, 1)
-	t.equal(r[0].name, i.name)
-	t.equal(r[0].tokens, i.tokens)
-	t.equal(r[0].relevance, 1/2 + .5/'Baz'.length)
-}))
-
-test('fuzzy: 1 fragment, 1 token, distance of 0', so(function* (t) {
-	const s = find.fuzzy(['foo'])
-	const i = {type: 'station', name: 'Foo', tokens: ['foo']}
-	const r = yield sink(s, i)
-	t.equal(r.length, 1)
-	t.equal(r[0].name, i.name)
-	t.equal(r[0].tokens, i.tokens)
-	t.equal(r[0].relevance, 1 + .5/'Foo'.length)
-}))
-
-
-
-test('find', (t) => {
-	t.plan(2)
-
-	find('U Alt-Mareindorf (Berlin)', find.fuzzy) // note the typo
+test('find – U mehringdamm', (t) => {
+	find('U mehringdamm', find.exact)
 	.on('data', (s) => {
-		if (s.id === '900000070301') {
-			t.equal(s.type, 'station')
+		t.equal(s.type, 'station')
+		if (s.id === '900000017101') {
 			t.pass('match')
+			t.end()
 		}
 	})
 })
